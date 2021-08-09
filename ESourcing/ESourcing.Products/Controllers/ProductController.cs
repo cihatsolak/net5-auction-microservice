3fa06e8558fb5441ccd9505e2fe5e726c81b108b
Cihat Solak <cihatsolak@hotmail.com>
2021-08-10 00:04:30 +0300

    product class creation and mongodb integration

---
+++ ESourcing/ESourcing.Products/Controllers/ProductController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ESourcing.Products.Controllers
+{
+    [Route("api/v1/[controller]/[action]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+    }
+}
