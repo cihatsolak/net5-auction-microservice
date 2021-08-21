ebbf7a2c9bd68547de28dcafa3288e0940a72521
Cihat Solak <cihatsolak@hotmail.com>
2021-08-21 17:17:56 +0300

    Order entity created for domain layer

---
+++ ESourcing/ESourcing.Order/Controllers/OrderController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ESourcing.Order.Controllers
+{
+    [Route("api/v1/[controller]/[action]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+    }
+}
