67719985db010f3aa59130af4e62c8ea6d0b1c6d
Cihat Solak <cihatsolak@hotmail.com>
2021-08-21 17:30:19 +0300

    Generic repository added

---
+++ ESourcing/Ordering.Domain/Repositories/IOrderRepository.cs
@@ -0,0 +1,12 @@
+﻿using Ordering.Domain.Entities;
+using Ordering.Domain.Repositories.Base;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.Repositories
+{
+    public interface IOrderRepository : IRepository<Order>
+    {
+        Task<IEnumerable<Order>> GetOrdersBySellerUserNameAsync(string userName);
+    }
+}
