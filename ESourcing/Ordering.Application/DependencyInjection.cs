e65c6b2a0fdd958db7a214c7d8274f10462e5b36
Cihat Solak <cihatsolak@hotmail.com>
2021-08-25 01:03:12 +0300

    Handlers and query methods for order

--- ESourcing/Ordering.Application/DependencyInjection.cs
+++ ESourcing/Ordering.Application/DependencyInjection.cs
@@ -1,6 +1,8 @@
-﻿using FluentValidation;
+﻿using AutoMapper;
+using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Mapper;
 using System.Reflection;
 
 namespace Ordering.Application
@@ -12,6 +14,15 @@ namespace Ordering.Application
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
             services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
             services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            #region Configure Mapper
+            var config = new MapperConfiguration(configure =>
+            {
+                configure.ShouldMapProperty = p => p.GetMethod.IsPublic || p.GetMethod.IsAssembly;
+                configure.AddProfile<OrderMappingProfile>();
+            });
+            var mapper = config.CreateMapper();
+            #endregion
         }
     }
 }
