a576de594be9ff47fe3848c6d3cf2d56d599dc25
Cihat Solak <cihatsolak@hotmail.com>
2021-08-24 22:58:55 +0300

    Order context added

--- ESourcing/ESourcing.Order/Startup.cs
+++ ESourcing/ESourcing.Order/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Ordering.Infrastructure;
 
 namespace ESourcing.Order
 {
@@ -15,10 +16,9 @@ namespace ESourcing.Order
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddInfrastructure(Configuration);
             services.AddControllers();
         }
 
---
+++ ESourcing/Ordering.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Infrastructure.Data;
+
+namespace Ordering.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<OrderContext>(opt => opt.UseInMemoryDatabase("InMemoryDb"), ServiceLifetime.Singleton, ServiceLifetime.Singleton);
+        }
+    }
+}
