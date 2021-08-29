6b25294c010905c64b129ce547fc1d21393c9a8f
Cihat Solak <cihatsolak@hotmail.com>
2021-08-29 22:48:23 +0300

    Order creation and customer's order list services were created.

--- ESourcing/ESourcing.Order/Controllers/OrderController.cs
+++ ESourcing/ESourcing.Order/Controllers/OrderController.cs
@@ -1,4 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Queries;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Collections;
+using Ordering.Application.Responses;
+using System.Collections.Generic;
+using System.Net;
+using Ordering.Application.Commands.OrderCreate;
 
 namespace ESourcing.Order.Controllers
 {
@@ -6,5 +16,43 @@ namespace ESourcing.Order.Controllers
     [ApiController]
     public class OrderController : ControllerBase
     {
+        #region Fields
+        private readonly IMediator _mediator;
+        private readonly ILogger<OrderController> _logger;
+        #endregion
+
+        #region Ctor
+        public OrderController(IMediator mediator, ILogger<OrderController> logger)
+        {
+            _mediator = mediator;
+            _logger = logger;
+        }
+        #endregion
+
+        #region Methods
+        [HttpGet("{userName:minlength(1)}")]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType(typeof(IEnumerable<OrderResponse>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetOrdersByUsername(string userName)
+        {
+            GetOrdersBySellerUsernameQuery getOrdersBySellerUsernameQuery = new(userName);
+            var orderResponseList = await _mediator.Send(getOrdersBySellerUsernameQuery);
+            if (orderResponseList is not null || !orderResponseList.Any())
+            {
+                _logger.LogWarning($"No order found for {userName}.");
+                return NotFound();
+            }
+
+            return Ok(orderResponseList);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(IEnumerable<OrderResponse>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> OrderCreate([FromBody] OrderCreateCommand orderCreateCommand)
+        {
+            var orderResponse = await _mediator.Send(orderCreateCommand);
+            return Ok(orderResponse);
+        }
+        #endregion
     }
 }
