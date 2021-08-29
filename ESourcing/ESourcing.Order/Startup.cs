4bf9b2d61a0f89eb619cbf26c0f83df1e5ee0aea
Cihat Solak <cihatsolak@hotmail.com>
2021-08-29 22:53:23 +0300

    Swagger implementation

--- ESourcing/ESourcing.Order/Startup.cs
+++ ESourcing/ESourcing.Order/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Ordering.Application;
 using Ordering.Infrastructure;
 
@@ -22,6 +23,17 @@ namespace ESourcing.Order
             services.AddControllers();
             services.AddInfrastructure(Configuration);
             services.AddApplication();
+
+            #region Swagger
+            services.AddSwaggerGen(setup =>
+            {
+                setup.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "ESourcing.Products",
+                    Version = "1.0.0"
+                });
+            });
+            #endregion
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -39,6 +51,9 @@ namespace ESourcing.Order
             {
                 endpoints.MapControllers();
             });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(setup => setup.SwaggerEndpoint("/swagger/v1/swagger.json", "ESourcing.Order v1"));
         }
     }
 }
