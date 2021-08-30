4b16c454a3299d0677032156b4acfb1bd7f244ad
Cihat Solak <cihatsolak@hotmail.com>
2021-08-30 20:03:38 +0300

    ocelot package added and configured.

--- ESourcing/ESourcing.APIGateway/Program.cs
+++ ESourcing/ESourcing.APIGateway/Program.cs
@@ -1,11 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ESourcing.APIGateway
 {
@@ -21,6 +16,10 @@ namespace ESourcing.APIGateway
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                })
+                .ConfigureAppConfiguration((hostBuilderContext, configurationBuilder) =>
+                {
+                    configurationBuilder.AddJsonFile("ocelot.json");
                 });
     }
 }
