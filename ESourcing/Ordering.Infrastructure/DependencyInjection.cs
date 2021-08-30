0cd7496d5fab96712f4d2160a3d8156757d7f25b
Cihat Solak <cihatsolak@hotmail.com>
2021-08-30 16:52:37 +0300

    Migrated from in-memory database to mssql

--- ESourcing/Ordering.Infrastructure/DependencyInjection.cs
+++ ESourcing/Ordering.Infrastructure/DependencyInjection.cs
@@ -13,9 +13,12 @@ namespace Ordering.Infrastructure
     {
         public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddDbContext<OrderContext>(opt => opt.UseInMemoryDatabase("InMemoryDb"), ServiceLifetime.Singleton, ServiceLifetime.Singleton);
+            //services.AddDbContext<OrderContext>(opt => opt.UseInMemoryDatabase("InMemoryDb"), ServiceLifetime.Singleton, ServiceLifetime.Singleton);
 
-            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            services.AddDbContext<OrderContext>(opt =>  opt.UseSqlServer(configuration.GetConnectionString("OrderApiConnection"),
+                                                sqlServerOptionsAction => sqlServerOptionsAction.MigrationsAssembly(typeof(OrderContext).Assembly.FullName)), ServiceLifetime.Singleton);
+
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>)); 
             services.AddScoped<IOrderRepository, OrderRepository>();
         }
     }
