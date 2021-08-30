8e71e2f88ba4e2968cd6b4b2edbfa2f0cd1f174b
Cihat Solak <cihatsolak@hotmail.com>
2021-08-30 20:42:58 +0300

    Static files added.

--- ESourcing/ESourcing.UI/Controllers/HomeController.cs
+++ ESourcing/ESourcing.UI/Controllers/HomeController.cs
@@ -1,11 +1,5 @@
-﻿using ESourcing.UI.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ESourcing.UI.Controllers
 {
@@ -21,17 +15,6 @@ namespace ESourcing.UI.Controllers
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
