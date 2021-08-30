59499200df08d3670959880192d6b735ae1d6bb1
Cihat Solak <cihatsolak@hotmail.com>
2021-08-30 14:19:33 +0300

    EventBus library file order consumer development on order microservice

--- ESourcing/ESourcing.Order/Startup.cs
+++ ESourcing/ESourcing.Order/Startup.cs
@@ -1,11 +1,16 @@
+using ESourcing.Order.Consumers;
+using ESourcing.Order.Extensions;
+using EventBusRabbitMQ;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Ordering.Application;
 using Ordering.Infrastructure;
+using RabbitMQ.Client;
 
 namespace ESourcing.Order
 {
@@ -34,6 +39,24 @@ namespace ESourcing.Order
                 });
             });
             #endregion
+
+            #region RabbitMQ
+            services.AddSingleton<IRabbitMQPersistentConnection>(provider =>
+            {
+                var logger = provider.GetRequiredService<ILogger<DefaultRabbitMQPersistentConnection>>();
+                ConnectionFactory connectionFactory = new()
+                {
+                    HostName = Configuration["EventBusSettings:HostName"],
+                    UserName = Configuration["EventBusSettings:Password"],
+                    Password = Configuration["EventBusSettings:Password"]
+                };
+                int retryCount = int.Parse(Configuration["EventBusSettings:RetryCount"]);
+
+                return new DefaultRabbitMQPersistentConnection(connectionFactory, logger, retryCount);
+            });
+
+            services.AddSingleton<EventBusOrderCreateConsumer>();
+            #endregion
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -51,6 +74,8 @@ namespace ESourcing.Order
             {
                 endpoints.MapControllers();
             });
+
+            app.UseRabbitListener();
 
             app.UseSwagger();
             app.UseSwaggerUI(setup => setup.SwaggerEndpoint("/swagger/v1/swagger.json", "ESourcing.Order v1"));
