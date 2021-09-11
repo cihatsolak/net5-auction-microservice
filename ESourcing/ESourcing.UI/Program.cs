9023e95c8a6fddbb8c0b0837f34eb9e563d64932
Cihat Solak <cihatsolak@hotmail.com>
2021-09-11 16:47:24 +0300

    Created sample user data for the project

--- ESourcing/ESourcing.UI/Program.cs
+++ ESourcing/ESourcing.UI/Program.cs
@@ -1,11 +1,9 @@
+using ESourcing.UI.Infrastructure.Data;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ESourcing.UI
 {
@@ -13,7 +11,9 @@ namespace ESourcing.UI
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            IHost host = CreateHostBuilder(args).Build();
+            CreateAndSeedDatabase(host);
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -22,5 +22,23 @@ namespace ESourcing.UI
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        private static void CreateAndSeedDatabase(IHost host)
+        {
+            using var serviceScope = host.Services.CreateScope();
+            var serviceProvider = serviceScope.ServiceProvider;
+            var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+
+            try
+            {
+                var webAppContext = serviceProvider.GetRequiredService<WebAppContext>();
+                WebAppContextSeed.SeedAsync(webAppContext, loggerFactory).Wait();
+            }
+            catch (Exception exception)
+            {
+                var logger = loggerFactory.CreateLogger<Program>();
+                logger.LogError(exception, "An error occured seeding the DB");
+            }
+        }
     }
 }
